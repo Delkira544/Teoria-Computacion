1e278bb6688713230899b0d61058ed9f1a77cfb4
Christian Verdugo <crischandel@gmail.com>
2025-09-06 01:10:55 -0400

    Revert "feat: Avance del analizador lexico"
    This reverts commit a2aa88ba6a7d2529ec4641b6f2dcb398277e4b33.

--- 2_Tarea/src/utils/file_handler.py
+++
@@ -1,7 +0,0 @@
-def read_file_utf8(path: str) -> str:
-    with open(path, "r", encoding="utf-8") as f:
-        return f.read()
-
-def save_file_utf8(path: str, content: str) -> None:
-    with open(path, "w", encoding="utf-8") as f:
-        f.write(content)
